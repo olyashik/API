f81ad61b0d406bf96b86b8401358895dd6df5db1
olyashik <yarvolya2005@mail.ru>
2025-08-14 13:46:39 +0300

    Edit files and remove .idea

--- little rotator/base_commands.py
+++ little rotator/base_commands.py
@@ -1,32 +1,6 @@
 from machine import Pin
+from utils import in1, in2, in3, in4, in5, in6, in7,in8, stepSequenceCW, stepSequenceCCW
 import time
-
-# Настройка пинов (Низ)
-in1 = Pin(10, Pin.OUT)  # IN1
-in2 = Pin(11, Pin.OUT)  # IN2
-in3 = Pin(12, Pin.OUT)  # IN3
-in4 = Pin(13, Pin.OUT)  # IN4
-
-# Настройка пинов (Верх)
-in5 = Pin(2, Pin.OUT)  # IN1
-in6 = Pin(3, Pin.OUT)  # IN2
-in7 = Pin(4, Pin.OUT)  # IN3
-in8 = Pin(5, Pin.OUT)  # IN4
-
-# Последовательность шагов для движения по часовой стрелке
-stepSequenceCW = [
-    [1, 0, 1, 0],
-    [0, 1, 1, 0],
-    [0, 1, 0, 1],
-    [1, 0, 0, 1]
-]
-# Последовательность шагов для движения против часовой стрелки
-stepSequenceCCW = [
-    [1, 0, 0, 1],
-    [0, 1, 0, 1],
-    [0, 1, 1, 0],
-    [1, 0, 1, 0]
-]
 
 # Функция для выполнения одного шага
 def step_motor_move(step, Time):

81ffca08425fafc47773a7be3c3766b6c04f67fb
olyashik <yarvolya2005@mail.ru>
2025-08-20 17:38:26 +0300

    продолжаем разделять

--- src/sshmind.py
+++
@@ -1,7 +0,0 @@
-# Тут могла бы быть реклама вашего кота, но вы так и не скинули его фото
-import sys
-
-if __name__ == "__main__":
-    data = sys.argv[1] if len(sys.argv) > 1 else None
-    if data:
-        print(f"Получены данные: {data}")
